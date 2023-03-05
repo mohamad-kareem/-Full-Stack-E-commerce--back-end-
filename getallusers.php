505153eda6e2ab9722db7af5eff8736f69804c15
HanadiAbouHamdan <hanadi.abouhamdan97@hotmail.com>
2023-03-05 13:18:16 +0200

    Adding API for users

---
+++ getallusers.php
@@ -0,0 +1,19 @@
+
+<?php
+include ('connection.php');
+
+// Prepare and execute the SQL query 
+$stmt = $mysqli->prepare("SELECT * FROM users");
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Fetch the results and store them in an array
+$all_users = array();
+while ($row = $result->fetch_assoc()) {
+    $all_users[] = $row;
+}
+
+// Send the response as JSON
+header('Content-Type: application/json');
+echo json_encode($all_users);
+?>
