93f12bfbd4f49a736f70e868c966f24c6c1e87ad
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-06 14:49:22 +0200

    Adding editphone Api

---
+++ editphone.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+include('connection.php');
+
+$phone_id = $_POST['phone_id'];
+$brand = $_POST['brand'];
+$model = $_POST['model'];
+$price= $_POST["price"];
+$amount = $_POST['amount'];
+$memory = $_POST['memory'];
+$imgurl = $_POST['imgurl'];
+
+$check_phone = $mysqli->prepare('select model from phones where id=?');
+$check_phone->bind_param('i', $phone_id);
+$check_phone->execute();
+$check_phone->store_result();
+$phone_doesnt_exists = $check_phone->num_rows();
+
+if ($phone_doesnt_exists <= 0) {
+    $response['status'] = "failed";
+} else {
+    $query = $mysqli->prepare('update phones set brand_id=?, model=?, price=?, amount=?, memory=?, imgurl=? where id=?');
+    $query->bind_param('isiiisi', $brand, $model, $price, $amount, $memory, $imgurl, $phone_id);
+    $query->execute();
+    $response['status'] = "success";
+}
+
+echo json_encode($response);
+?>
